7824c335df49ad09a9c7c0b18ba174a8b48a176d
ivan Rodriguez <ivanwork003@gmail.com>
2025-04-24 00:51:17 -0400

    versión inicial de App

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'screens/home_screen.dart';
+
+void main() {
+  runApp(const BefineApp());
+}
+
+class BefineApp extends StatelessWidget {
+  const BefineApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Agua Purificada Befine Corp',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
